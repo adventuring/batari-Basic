379b9830f57c48165173b02484d9a9459bd22511
Bruce-Robert Pocock <brpocock@star-hope.org>
2025-11-14 19:16:23 -0800

    Always use Bank 2 method (subtract physical base) - check data length range 0-4096
    Always subtract physical base for all banks. If data length is in range 0-4096, use it; otherwise treat as 0.
    
    Bank 2 works correctly (1456 data), Bank 16 works (6445 data), but all other banks show 0 data - suggesting labels resolve differently for different banks. The condition checking if data length is in range 0-4096 is failing for most banks.

--- statements.c
+++ statements.c
@@ -1341,37 +1341,27 @@ void newbank(int bankno)
 	    /* Set RORG to $F000 before evaluating labels to ensure correct CPU relocatable address context */
 	    printf(" RORG $F000\n");
 	    printf(" ifconst bscode_length\n");
-	    /* Check if labels include physical offset (> $FFFF) - if so, subtract physical base (Bank 2 method) */
-	    printf("  if Bank%dCodeEnds > $FFFF\n", prev_bank);
-	    /* Labels include physical offset - subtract physical base (Bank 2 case) */
-	    printf("   if (Bank%dCodeEnds - $%04X) > ($FFE0 - bscode_length)\n", prev_bank, prev_bank_phys_base);
+	    /* Always use Bank 2 method (subtract physical base) for all banks */
+	    /* Check overflow first, then check if data length is reasonable (0-4096) */
+	    printf("  if (Bank%dCodeEnds - $%04X) > ($FFE0 - bscode_length)\n", prev_bank, prev_bank_phys_base);
+	    /* Overflow case - check if data length is reasonable (0-4096), otherwise treat as 0 */
+	    printf("   if (((Bank%dDataEnds - $%04X) - $F100) >= 0) && (((Bank%dDataEnds - $%04X) - $F100) < 4096)\n", 
+		   prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
 	    printf("    echo \"Bank %d: \", [(Bank%dDataEnds - $%04X) - $F100]d, \" data, \", [(Bank%dCodeEnds - $%04X) - (Bank%dDataEnds - $%04X)]d, \" code, \", [bscode_length]d, \" bscode \", [(Bank%dCodeEnds - $%04X) - ($FFE0 - bscode_length)]d, \" bytes OVERFLOW\"\n",
 		   prev_bank, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
 	    printf("   else\n");
-	    printf("    echo \"Bank %d: \", [(Bank%dDataEnds - $%04X) - $F100]d, \" data, \", [(Bank%dCodeEnds - $%04X) - (Bank%dDataEnds - $%04X)]d, \" code, \", [bscode_length]d, \" bscode \", [($FFE0 - bscode_length) - (Bank%dCodeEnds - $%04X)]d, \" free bytes\"\n",
-		   prev_bank, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
+	    printf("    echo \"Bank %d: \", [0]d, \" data, \", [(Bank%dCodeEnds - $%04X) - (Bank%dDataEnds - $%04X)]d, \" code, \", [bscode_length]d, \" bscode \", [(Bank%dCodeEnds - $%04X) - ($FFE0 - bscode_length)]d, \" bytes OVERFLOW\"\n",
+		   prev_bank, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
 	    printf("   endif\n");
 	    printf("  else\n");
-	    /* Labels are <= $FFFF - may be offsets (0-$FFF) or absolute ($F000-$FFFF) */
-	    /* If >= $F000, already absolute; otherwise add $F000 to get absolute CPU address */
-	    printf("   if Bank%dDataEnds >= $F000\n", prev_bank);
-	    /* Already absolute - use directly */
-	    printf("    if Bank%dCodeEnds > ($FFE0 - bscode_length)\n", prev_bank);
-	    printf("     echo \"Bank %d: \", [Bank%dDataEnds - $F100]d, \" data, \", [Bank%dCodeEnds - Bank%dDataEnds]d, \" code, \", [bscode_length]d, \" bscode \", [Bank%dCodeEnds - ($FFE0 - bscode_length)]d, \" bytes OVERFLOW\"\n",
-		   prev_bank, prev_bank, prev_bank, prev_bank, prev_bank, prev_bank);
-	    printf("    else\n");
-	    printf("     echo \"Bank %d: \", [Bank%dDataEnds - $F100]d, \" data, \", [Bank%dCodeEnds - Bank%dDataEnds]d, \" code, \", [bscode_length]d, \" bscode \", [($FFE0 - bscode_length) - Bank%dCodeEnds]d, \" free bytes\"\n",
-		   prev_bank, prev_bank, prev_bank, prev_bank, prev_bank, prev_bank);
-	    printf("    endif\n");
+	    /* No overflow - check if data length is reasonable (0-4096), otherwise treat as 0 */
+	    printf("   if (((Bank%dDataEnds - $%04X) - $F100) >= 0) && (((Bank%dDataEnds - $%04X) - $F100) < 4096)\n", 
+		   prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
+	    printf("    echo \"Bank %d: \", [(Bank%dDataEnds - $%04X) - $F100]d, \" data, \", [(Bank%dCodeEnds - $%04X) - (Bank%dDataEnds - $%04X)]d, \" code, \", [bscode_length]d, \" bscode \", [($FFE0 - bscode_length) - (Bank%dCodeEnds - $%04X)]d, \" free bytes\"\n",
+		   prev_bank, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
 	    printf("   else\n");
-	    /* Offset (0-$FFF) - add $F000 to get absolute CPU address */
-	    printf("    if (Bank%dCodeEnds + $F000) > ($FFE0 - bscode_length)\n", prev_bank);
-	    printf("     echo \"Bank %d: \", [(Bank%dDataEnds + $F000) - $F100]d, \" data, \", [(Bank%dCodeEnds + $F000) - (Bank%dDataEnds + $F000)]d, \" code, \", [bscode_length]d, \" bscode \", [(Bank%dCodeEnds + $F000) - ($FFE0 - bscode_length)]d, \" bytes OVERFLOW\"\n",
-		   prev_bank, prev_bank, prev_bank, prev_bank, prev_bank, prev_bank);
-	    printf("    else\n");
-	    printf("     echo \"Bank %d: \", [(Bank%dDataEnds + $F000) - $F100]d, \" data, \", [(Bank%dCodeEnds + $F000) - (Bank%dDataEnds + $F000)]d, \" code, \", [bscode_length]d, \" bscode \", [($FFE0 - bscode_length) - (Bank%dCodeEnds + $F000)]d, \" free bytes\"\n",
-		   prev_bank, prev_bank, prev_bank, prev_bank, prev_bank, prev_bank);
-	    printf("    endif\n");
+	    printf("    echo \"Bank %d: \", [0]d, \" data, \", [(Bank%dCodeEnds - $%04X) - (Bank%dDataEnds - $%04X)]d, \" code, \", [bscode_length]d, \" bscode \", [($FFE0 - bscode_length) - (Bank%dCodeEnds - $%04X)]d, \" free bytes\"\n",
+		   prev_bank, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base, prev_bank, prev_bank_phys_base);
 	    printf("   endif\n");
 	    printf("  endif\n");
 	    printf(" else\n");
